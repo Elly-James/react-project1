cf8d001cb22b377915e75cdeb63f9b6e672f6954
Elly <ellyjames1999@gmail.com>
2025-04-10 19:20:27 +0300

    added google login

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
+import { GOOGLE_CLIENT_ID } from './config/auth';
 import './app.css';
 import Navbar from './Components/Navbar/Navbar.jsx';
 import Home from './Components/Home/Home.jsx';
@@ -12,17 +14,19 @@ import Summary from '../src/Components/summary/Summary.jsx';
 
 const App = () => {
   return (
-    <Router>
-      <Navbar />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/kenyan-holidays" element={<KenyanHolidays />} />
-        <Route path="/international-holidays" element={<InternationalHolidays />} />
-        <Route path="/mycart" element={<MyCart />} />
-        <Route path="/summary" element={<Summary />} />
-      </Routes>
-      <Footer />
-    </Router>
+    <GoogleOAuthProvider clientId={GOOGLE_CLIENT_ID}>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/kenyan-holidays" element={<KenyanHolidays />} />
+          <Route path="/international-holidays" element={<InternationalHolidays />} />
+          <Route path="/mycart" element={<MyCart />} />
+          <Route path="/summary" element={<Summary />} />
+        </Routes>
+        <Footer />
+      </Router>
+    </GoogleOAuthProvider>
   );
 };
 
